4dd7e988d96687481e11073550ec2bfcd440ecdf
T G <512747@cognizant.com>
2020-03-06 16:24:18 +0530

    changes for distributed code

--- integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
+++ integration-test/src/test/java/test/pivotal/pal/tracker/FlowTest.java
@@ -158,14 +158,5 @@ public class FlowTest {
         response = httpClient.get(timesheetsServerUrl("/time-entries?userId=" + createdUserId));
         assertThat(findResponseId(response)).isEqualTo(createdTimeEntryId);
     }
-    public void startWithDatabaseName(String dbName) throws IOException, InterruptedException {
-        String dbUrl = "jdbc:mysql://localhost:3306/" + dbName + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false";
 
-        start(envMapBuilder()
-                .put("SPRING_DATASOURCE_URL", dbUrl)
-                .put("EUREKA_CLIENT_ENABLED", "false")
-                .put("RIBBON_EUREKA_ENABLED", "false")
-                .put("REGISTRATION_SERVER_RIBBON_LISTOFSERVERS", "http://localhost:8883")
-                .build());
-    }
 }
